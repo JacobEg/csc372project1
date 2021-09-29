4859e3329cf68ae27999be3aaf7b216cabea481f
cademarks <DickensConklinMisurda2332>
2021-09-28 20:07:29 -0700

    Added comments and methods, fixed class removal bug.

--- SchoolSystem/src/SchoolTest.kt
+++ SchoolSystem/src/SchoolTest.kt
@@ -1,3 +1,8 @@
+/**
+ * SchoolTest.kt
+ *
+ * Used to test the SchoolClass, Person, Professor, and Student Classes. May be modified to test different methods.
+ */
 fun main() {
     val jake = Student("Jake", 555111)
     val bob = Student("Bob", 124124)
@@ -6,12 +11,13 @@ fun main() {
     val joe = Professor("Joe", 5698)
     val csc101 = SchoolClass(1261, "CSC 101", mary)
     val csc110 = SchoolClass(1262, "CSC 110", joe)
+    jake.introduce() // inherited method from Person superclass
     csc101.addStudent(jake)
     csc101.addStudent(bob)
     csc110.addStudent(jake)
     csc110.addStudent(bob)
     csc110.addStudent(susan)
-    println(bob.toString())
+    println(bob.toString()) // Kotlin print method
     println(csc110.toString())
     csc110.removeStudent(bob.getID())
     println(bob.toString())
